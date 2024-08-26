ac75eb3240d44d13f87bd3b26f7912492bacb984
ZoyaYu <zoya.y@deskalerts.com>
2024-08-26 11:42:12 +0300

    Allure

--- tests/test_get_resource_data.py
+++ tests/test_get_resource_data.py
@@ -1,6 +1,6 @@
 import httpx
 from jsonschema import validate
-
+import allure
 from core.contracts import RESOURCE_DATA_SCHEME
 
 BASE_URL = "https://reqres.in/"
@@ -8,23 +8,35 @@ LIST_RESOURCES = "api/unknown"
 SINGLE_RESOURCE = "api/unknown/2"
 NOT_FOUND_RESOURCE = "api/unknown/23"
 
+@allure.suite('Проверка запросов данных ресурсов')
+@allure.title('Проверяем получение списка ресурсов')
+
 def test_list_resources():
-    response = httpx.get(BASE_URL + LIST_RESOURCES)
-    assert response.status_code == 200
+    with allure.step(f'Делаем запрос по адресу:{BASE_URL + LIST_RESOURCES}'):
+        response = httpx.get(BASE_URL + LIST_RESOURCES)
+    with allure.step('Проверяем код ответа'):
+        assert response.status_code == 200
     data = response.json()['data']
 
     for item in data:
-        validate(item, RESOURCE_DATA_SCHEME)
-        assert item['color'].startswith('#')
+        with allure.step('Проверяем элемент из списка'):
+            validate(item, RESOURCE_DATA_SCHEME)
+            with allure.step('Проверяем начало поля Color'):
+                assert item['color'].startswith('#')
 
 def test_single_resource():
-    response = httpx.get(BASE_URL + SINGLE_RESOURCE)
-    assert response.status_code == 200
+    with allure.step(f'Делаем запрос по адресу:{BASE_URL + SINGLE_RESOURCE}'):
+        response = httpx.get(BASE_URL + SINGLE_RESOURCE)
+    with allure.step('Проверяем код ответа'):
+        assert response.status_code == 200
     data = response.json()['data']
 
     validate(data, RESOURCE_DATA_SCHEME)
-    assert data['color'].startswith('#')
+    with allure.step('Проверяем начало поля Color'):
+        assert data['color'].startswith('#')
 
 def test_resource_not_found():
-    response = httpx.get(BASE_URL + NOT_FOUND_RESOURCE)
-    assert response.status_code == 404
+    with allure.step(f'Делаем запрос по адресу:{BASE_URL + NOT_FOUND_RESOURCE}'):
+        response = httpx.get(BASE_URL + NOT_FOUND_RESOURCE)
+    with allure.step('Проверяем код ответа'):
+        assert response.status_code == 404
